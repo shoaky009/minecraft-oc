eb934a095ea0901af5418077e9c90b06e887d4a3
shoaky <shoaky159>
2021-01-23 13:36:10 +0800

    update

--- bacterial-cultures/src/main.lua
+++ bacterial-cultures/src/main.lua
@@ -9,19 +9,30 @@ local interval = 1
 
 local expectAmount = tp.getTankCapacity(side, 1) / 2
 
-local sourceTimer = event.timer(interval, Main.loop, math.huge)
+function Main.start()
+    local sourceTimer = event.timer(interval, Main.loop, math.huge)
+    print("started!")
+    while true do
+        local id, _, x, y = event.pullMultiple("interrupted")
+        if id == "interrupted" then
+            print("interrupted cancel timer")
+            event.cancel(sourceTimer)
+            break
+        end
+    end
+end
 
 function Main.loop()
     local currentAmount = tp.getTankLevel(side, 1)
     if currentAmount > expectAmount then
         local tpAmount = currentAmount - expectAmount
         local transferred = 0
-        while transferred >= tpAmount do
+        while transferred <= tpAmount do
             local _, amount = tp.transferFluid(side, output, tpAmount)
             transferred = transferred + amount
+            os.sleep(0.1)
         end
     end
 end
 
-return Main
-
+Main.start()
