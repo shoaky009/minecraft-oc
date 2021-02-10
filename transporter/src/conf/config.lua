65972133b23d4971278ced8159f4ac1267b4945e
shoaky <shoaky159>
2021-02-10 17:13:09 +0800

    tp

--- transporter/src/conf/config.lua
+++ transporter/src/conf/config.lua
@@ -2,20 +2,64 @@ local sides = require("sides")
 local _M = {
 }
 
+_M.checkInterval = 0.2
+_M.inputSide = sides.north
+_M.outputSide = sides.east
+
 local items = {}
-items["gt.metaitem.01.2921.name"] = { amount = 9 }
-items["gt.metaitem.01.2831.name"] = { amount = 20 }
 items["氯化钙粉"] = { amount = 3 }
-items["gt.metaitem.01.2842.name"] = { amount = 20 }
 items["钙铁辉石粉"] = { amount = 10 }
 items["斑铜粉"] = { amount = 10 }
 items["久辉铜粉"] = { amount = 47 }
-
+items["硫酸锌粉"] = { amount = 6 }
+items["绿铬云母粉"] = { amount = 21 }
+items["砷黄铁粉"] = { amount = 3 }
+items["钨酸锰粉"] = { amount = 5 }
+items["钨铁粉"] = { amount = 5 }
+items["铝电气石粉"] = { amount = 51 }
+items["辉铋粉"] = { amount = 5 }
+items["碲汞钯粉"] = { amount = 5 }
+items["泡铋粉"] = { amount = 3 }
+items["硫铟铜粉"] = { amount = 4 }
+items["氟铁电气石粉"] = { amount = 50 }
+items["gt.metaitem.01.2831.name"] = { amount = 20 }
+items["gt.metaitem.01.2921.name"] = { amount = 9 }
+items["gt.metaitem.01.2842.name"] = { amount = 20 }
+items["gt.metaitem.01.2833.name"] = { amount = 5 }
+items["gt.metaitem.01.2937.name"] = { amount = 1 }
+items["gt.metaitem.01.2839.name"] = { amount = 2 }
+items["gt.metaitem.01.2824.name"] = { amount = 1 }
+items["gt.metaitem.01.2904.name"] = { amount = 6 }
+items["gt.metaitem.01.2607.name"] = { amount = 11 }
+items["gt.metaitem.01.2847.name"] = { amount = 13 }
+items["gt.metaitem.01.2841.name"] = { amount = 7 }
+items["gt.metaitem.01.2838.name"] = { amount = 20 }
+items["gt.metaitem.01.2822.name"] = { amount = 39 }
+items["gt.metaitem.01.2676.name"] = { amount = 1 }
+items["gt.metaitem.01.2922.name"] = { amount = 3 }
+items["gt.metaitem.01.2848.name"] = { amount = 22 }
+items["gt.metaitem.01.2923.name"] = { amount = 2 }
+items["gt.metaitem.01.2871.name"] = { amount = 10 }
+items["gt.metaitem.01.2525.name"] = { amount = 11 }
+items["gt.metaitem.01.2917.name"] = { amount = 5 }
+items["gt.metaitem.01.2538.name"] = { amount = 4 }
+items["gt.metaitem.01.2910.name"] = { amount = 7 }
+items["gt.metaitem.01.2845.name"] = { amount = 8 }
+items["gt.metaitem.01.2919.name"] = { amount = 11 }
+items["gt.metaitem.01.2508.name"] = { amount = 22 }
+items["gt.metaitem.01.2835.name"] = { amount = 20 }
+items["gt.metaitem.01.2929.name"] = { amount = 17 }
+items["gt.metaitem.01.2883.name"] = { amount = 6 }
+items["gt.metaitem.01.2908.name"] = { amount = 5 }
+items["gt.metaitem.01.2927.name"] = { amount = 30 }
+items["gt.metaitem.01.2820.name"] = { amount = 20 }
+items["gt.metaitem.01.2905.name"] = { amount = 6 }
+items["Kashinite Dust"] = { amount = 7 }
+items["Demicheleite (Br) Dust"] = { amount = 25 }
+items["Alburnite Dust"] = { amount = 15 }
+items["Florencite Dust"] = { amount = 21 }
+items["Agardite (La) Dust"] = { amount = 38 }
+items["Lautarite Dust"] = { amount = 9 }
 _M.items = items
-_M.checkInterval = 0.2
-_M.fixInterval = 1
-_M.inputSide = sides.north
-_M.outputSide = sides.east
-_M.fixOutputSide = 1
 
 return _M
--- transporter/src/main.lua
+++ transporter/src/main.lua
@@ -5,54 +5,39 @@ local event = require("event")
 
 local inputSide = config.inputSide
 local outputSide = config.outputSide
-local fixOutSide = config.fixOutputSide
 local items = config.items
 
 function Main.start()
     local interval = config.checkInterval or 0.5
     local sourceTimer = event.timer(interval, Main.loop, math.huge)
-    local fixInterval = config.fixInterval or 2
-    local fixTimer = event.timer(fixInterval, Main.fixInput, math.huge)
     print("started!")
     while true do
         local id, _, x, y = event.pullMultiple("interrupted")
         if id == "interrupted" then
             print("interrupted cancel timer")
             event.cancel(sourceTimer)
-            event.cancel(fixTimer)
             break
         end
     end
 end
 
 function Main.loop()
+    local outputSlot = tp.getStackInSlot(4, 1)
+    if outputSlot and outputSlot.size > 0 then
+        return
+    end
     local allStack = tp.getAllStacks(inputSide)
     if not allStack then
-        print("no stack info..")
         return
     end
 
-    local outputSlot = Main.getAvailableOutputSlot(tp, outputSide)
-    if not outputSlot then
-        print("no available outputSlot..")
-        return
-    end
     for i, v in pairs(allStack.getAll()) do
         local item = items[v.label]
         if item then
             if item.amount <= v.size then
-                tp.transferItem(inputSide, outputSide, Main.getNumber(item, v), i + 1, outputSlot)
+                tp.transferItem(inputSide, outputSide, Main.getNumber(item, v), i + 1, 1)
+                break
             end
-        end
-    end
-end
-
-function Main.getAvailableOutputSlot(transposer, side)
-    local size = transposer.getInventorySize(side)
-    for i = 1, size do
-        local stack = transposer.getStackInSlot(side, i)
-        if stack == nil then
-            return i
         end
     end
 end
@@ -66,24 +51,6 @@ function Main.getNumber(item, stackItem)
         return stackItem.size - mod
     end
     return stackItem.size
-end
-
-function Main.fixInput()
-    local allStack = tp.getAllStacks(outputSide)
-    if not allStack then
-        return
-    end
-
-    for i, v in pairs(allStack.getAll()) do
-        if v then
-            local item = items[v.label]
-            if item and v.size < 64 and v.size % item.amount > 0 then
-                local outputSlot = Main.getAvailableOutputSlot(tp, fixOutSide)
-                print("extract " .. v.label .. " size:" .. v.size .. " amount:" .. item.amount)
-                tp.transferItem(outputSide, fixOutSide, v.size, i + 1, outputSlot)
-            end
-        end
-    end
 end
 
 Main.start()
